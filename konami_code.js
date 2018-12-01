7d57d24b2fa99f15b4c11a7c1397ae3416eb270d
AAM77 <Adeel.A.Mhd@gmail.com>
2018-11-30 22:07:10 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let newCode = code.map(e => e);
 
+  window.addEventListener('keydown', function(e) {
+    let key = parseInt(e.detail || e.which);
+
+    if (key === newCode[0]) {
+      newCode.shift()
+      //console.log(key)
+
+      if (newCode.length === 0) {
+        alert("Hurray!")
+        //console.log("Hurray! You did it!")
+      }
+    } else {
+      newCode = code.map(e => e);
+      //console.log(key)
+    }
+  });
 }
